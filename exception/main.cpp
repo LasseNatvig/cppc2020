d521406f2ffccf39fdb7fa952a47cdfa4d60c45a
LasseNatvig <lasse.natvig@ntnu.no>
2019-11-22 16:55:40 +0100

    exception

---
+++ exception/main.cpp
@@ -0,0 +1,21 @@
+//
+// This is example code from Chapter 2.2 "The classic first program" of
+// "Programming -- Principles and Practice Using C++" by Bjarne Stroustrup
+// 
+// keep_window_open() added for TDT4102, excercise 0
+
+// This program outputs the message "Hello, World!" to the monitor
+
+#include "std_lib_facilities.h"
+
+//------------------------------------------------------------------------------'
+
+// C++ programs start by executing the function main
+int main()
+{
+	cout << "Hello, World!\n";
+
+	keep_window_open();
+}
+
+//------------------------------------------------------------------------------
