ca9824302f71b27c70f0053e2ff0c559d6c84281
LasseNatvig <lasse.natvig@ntnu.no>
2020-03-24 15:54:40 +0100

    filename change

--- testing/testing.cpp
+++ testing/testing.cpp
@@ -3,20 +3,60 @@
 // "Programming -- Principles and Practice Using C++" by Bjarne Stroustrup
 #include <algorithm>
 #include <iostream>
-#include<fstream>
+#include <fstream>
 #include <iterator>
 #include <string>
 #include <vector>
 using namespace std;
 
---- legg inn min egen binSearch --- bruk fra Einar Johan  const iterator ...  /// gå over all kode, forenkle ... 
+// --- legg inn min egen binSearch --- bruk fra Einar Johan  const iterator ...  /// gå over all kode, forenkle ... 
+
+// Since binary_search() is in the standard library we define our own bin_search() to
+// be able to inject errors for demonstrating the testing. 
+
+bool bin_search(const vector<int>::const_iterator& start, const vector<int>::const_iterator& end, int value) {
+	if (start != end) {
+		auto mid_point = (end - start) / 2;
+		auto mid_value = *(start + mid_point);
+		if (value < mid_value) {
+			return bin_search(start, end - mid_point, value);
+		}
+        else if (value > mid_value) {
+			return bin_search(start + mid_point, end, value);
+		} 
+        else if (mid_value == value) {
+			return true;
+		}
+        else {
+			return false;
+		} 
+	}
+}
+// EJAA-variant
+// bool bin_search(const vector<int>::const_iterator& start, const vector<int>::const_iterator& end, int value) {
+// 	if (start != end) {
+// 		auto mid_point = (end - start) / 2;
+// 		auto mid_value = *(start + mid_point);
+// 		if (mid_value == value) {
+// 			return true;
+// 		} else if (mid_point == 0) {
+// 			return false;
+// 		} else if (value > mid_value) {
+// 			return bin_search(start + mid_point, end, value);
+// 		} else if (value < mid_value) {
+// 			return bin_search(start, end - mid_point, value);
+// 		}
+// 		return true;
+// 	}
+// 	return false;
+// }
 
 
 struct Test {
     string label;
     int val;
     vector<int> seq;
-    bool res;
+    bool res; 
 };
 istream& operator>>(istream& is, Test& t) { // use the described format 
     // Example input: { 2-7 7 { 1 2 3 5 8 13 21} 0 }
@@ -40,7 +80,6 @@ istream& operator>>(istream& is, Test& t) { // use the described format
     //cout << c << '|' << t.res << '|' << d << '|' ;
     return is;
 }
-
 ostream& operator<<(ostream& os, const Test& t) {
     os << "{ " << t.label << ' ' << t.val << " { ";
     std::copy(t.seq.begin(), t.seq.end(), std::ostream_iterator<int>(os," "));
@@ -57,7 +96,7 @@ int test_all(string testFileName)  {
     Test t;
     while (tests>>t) {
         cout << t << endl;
-        bool r = binary_search( t.seq.begin(), t.seq.end(), t.val);
+        bool r = bin_search( t.seq.begin(), t.seq.end(), t.val);
         if (r !=t.res) {
             cout << "failure: test " << t.label
                  << " binary_search: "
@@ -69,10 +108,23 @@ int test_all(string testFileName)  {
     return error_count;
 }
 
-int main() {
+int main()
+try {
     string testFile{"BinarySearchTests.txt"}; 
     cout << "Running all tests from " + testFile + "\n";
     int errors = test_all(testFile);
     cout << "number of errors: " << errors << "\n";
     return 0;
+} catch (exception e) {
+    cerr << "Exception caught:" << e.what() << endl;
+    return -1;
 }
+
+
+// int main() {
+//     string testFile{"BinarySearchTests.txt"}; 
+//     cout << "Running all tests from " + testFile + "\n";
+//     int errors = test_all(testFile);
+//     cout << "number of errors: " << errors << "\n";
+//     return 0;
+// }
