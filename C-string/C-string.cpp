6f27b8b89feab746bb988210ffca15d0a024f923
LasseNatvig <lasse.natvig@ntnu.no>
2020-03-27 13:10:51 +0100

    update with references

--- C-string/C-string.cpp
+++ C-string/C-string.cpp
@@ -3,7 +3,7 @@
 #include <cstring>
 using namespace std;
 // comment the next line when compiling for MAC or Unix
-#define MS_VS 
+#define WINDOWS 
 
 int main() {
 	char a[10] = "ole";  // with extra unused space
@@ -13,21 +13,27 @@ int main() {
 
 	cout << "\n*** C-string copy\n";
 	strcpy(a, b);  // VS Code gives warning and suggests strcpy_s 
-#ifdef MS_VS
-	strcpy_s(a, b);
-	//strcpy_s(a2, b); // Gives runtime error: "L buffer is too small..."
+
+// In general, the availability of these functions differ between operating systems and libraries.
+// More info is here: https://en.cppreference.com/w/c/string/byte/strcpy	
+#ifdef WINDOWS
+	strcpy_s(a, b);  // is part of C11, only available under windows
+	// strcpy_s(a2, b); // Gives runtime error: "L buffer is too small..."
 #else
 	strcpy(a, b);
 	strcpy(a2, b);
 #endif
+
 	cout << "a = " << a << endl;
 	cout << "b = " << b << endl;
 
 	char c[] = "ccccc";
 	char d[] = "dddDDDDD";
 	cout << "d = " << d << endl;
-	// strncpy(a,b,n), copies n chars from the start of b to a (from the start of a)
-#ifdef MS_VS
+
+	// strncpy(a,b,n); copies n chars from the start of b to a (from the start of a), gives warning under windows
+	// More info: https://en.cppreference.com/w/c/string/byte/strncpy
+#ifdef WINDOWS
 	strncpy_s(d, c, 4);
 #else
 	strncpy(d, c, 4);
@@ -40,7 +46,9 @@ int main() {
 	char s3[20] = "Ole";
 	cout << "s1 = " << s1 << endl << "s2 = " << s2 << endl;
 
-#ifdef MS_VS
+// More info: https://en.cppreference.com/w/c/string/byte/strcat
+// More info: https://en.cppreference.com/w/c/string/byte/strncat
+#ifdef WINDOWS
 	strcat_s(s3, " ");  // appends a blank to s3
 	strcat_s(s3, s2); // appends s2 to s3
 	cout << "s3 = " << s3 << endl;
@@ -55,6 +63,7 @@ int main() {
 	strncat(s3, s2, 3);
 	cout << "s3 = " << s3 << endl;
 #endif
+
 	// search after char
 	// strchr returns pointer to the first instance of a char 
 	cout << "\n *** Search after char\n";
